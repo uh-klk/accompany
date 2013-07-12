72c7fede958b205daedbb38fcd709628fd3705f8
Joe <joe@joe-desktop.(none)>
2013-07-12 16:27:01 +0100

    With Sienna stuff

---
+++ accompany_context_aware_planner/build/CMakeFiles/getPotentialProxemicsLocations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "../srv_gen"
+  "../src/accompany_context_aware_planner/srv"
+  "CMakeFiles/getPotentialProxemicsLocations.dir/ros/src/getPotentialProxemicsLocations.cpp.o"
+  "../bin/getPotentialProxemicsLocations.pdb"
+  "../bin/getPotentialProxemicsLocations"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/getPotentialProxemicsLocations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
